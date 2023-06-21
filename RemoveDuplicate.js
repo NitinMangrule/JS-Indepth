cd8227c967c0ef9ced1e5a62ceed009cb923fcf5
Nitin Mangrule <ndmangrule@gmail.com>
2023-06-21 20:03:55 +0530

    Update RemoveDuplicate.js

--- RemoveDuplicate.js
+++ RemoveDuplicate.js
@@ -1,3 +1,18 @@
-function removeDuplicate(){
+// Remove duplicate alphabets from given string
 
-}
+const removeDuplicate = (inputString) => {
+    const output = new Set();
+
+    if (typeof inputString !== 'string') {
+        return 'Please provide a string.';
+    }
+
+    for (let i = 0; i < inputString.length; i++) {
+        output.add(inputString[i]);
+    }
+
+    return Array.from(output).join('');
+};
+
+removeDuplicate('racecar');
+removeDuplicate('122222racecar');
