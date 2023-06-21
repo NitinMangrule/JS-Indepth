981226fd88b905080e870e4877692f25f3789dda
Nitin Mangrule <ndmangrule@gmail.com>
2023-06-21 20:31:12 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,20 @@
 
+// Check whether given string is palindrome or not.
+
+const isPalindrome = (str) => {
+    if (typeof str !== 'string') {
+        return 'Please provide valid string input.';
+    }
+    const lowerCaseStr = str.toLowerCase();
+    const updatedStr = lowerCaseStr.replace(/[^a-z]/g, '');
+    
+
+    for (let i = 0; i < updatedStr.length / 2; i++) {
+        if (updatedStr[i] !== updatedStr[updatedStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPalindrome('racecar')); 
