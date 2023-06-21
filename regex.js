48f1d20774eaa054b54f5f577ab50bde378bd597
Nitin Mangrule <ndmangrule@gmail.com>
2023-06-21 21:21:55 +0530

    Create regex.js

---
+++ regex.js
@@ -0,0 +1,8 @@
+
+// for removing special charater
+'T*G*( &^**(1 11'.replace(/[^\w\s]/g,'')
+
+// for keeping small letter
+'T*Gss*( &^**(1 11'.replace(/[^a-z]/g,'')
+// Both
+'T*G*( &^**(1 11'.replace(/[^a-zA-Z]/g,'')
